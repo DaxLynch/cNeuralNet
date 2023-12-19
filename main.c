186afa2a615b344e97436d133b5fcfbe8a343368
Dax <Declanxlynch@gmail.com>
2023-12-19 15:34:50 -0800

    Finilized

--- main.c
+++ main.c
@@ -1,27 +1,30 @@
 #include "main.h"
-
+#define print 1
 int main(){
 	
 	setlocale(LC_CTYPE, "");
-	network net;
-
 	initMat();
-	networkAllocate(&net,3, 784, 30, 10);
-	networkWeightsInit(&net);
+	
+	
+	network net;
+	networkAllocate(&net,3, 784, 30, 10); //Initilizes a 3 layer net with sizes 784, 30, 100
+	networkWeightsInit(&net); //Sets the weights and biases to gaussian distributed around 0
 	
 	data* trainingData;
 	data* testingData;
-	int trainingLength = 1000;
-	int testingLength = 1000;
+	int trainingLength = 60000;
+	int testingLength = 10000;
 	dataLoader(&trainingData, "trainingData/train-images.idx3-ubyte", "trainingData/train-labels.idx1-ubyte", trainingLength);
 	dataLoader(&testingData, "trainingData/t10k-images.idx3-ubyte", "trainingData/t10k-labels.idx1-ubyte", testingLength);
+	//The above loades data in from the trainingData folder,
 	
-	networkSGD(&net, trainingData, trainingLength, testingData, testingLength, 1,1, 100, 5);
+	networkSGD(&net, trainingData, trainingLength, testingData, testingLength, print, 10, 10, 5);
+	//The above does SGD on the training data, with it printing every epoch, and 10 epochs, and a batchsize of 10, with a learning rate of 5.
 	
-	evaluateSetManual(&net, testingData, testingLength);
-	evaluateSetFailures(&net, testingData, testingLength);
+	evaluateSetManual(&net, testingData, testingLength); //This prints the data, alongside the returned value and the truth value
+	evaluateSetFailures(&net, testingData, testingLength); //Like above but it shows you the failures
 
-	networkFree(&net);
+	networkFree(&net);//These commands free the networks and arrays
 	dataFree(&trainingData, trainingLength);
 	dataFree(&testingData, testingLength);
 
